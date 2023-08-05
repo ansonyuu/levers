fa42b5dd8152c5c903cc367fd7ca99a496f4aa75
ansonyuu <ansonyu24@gmail.com>
2023-08-05 10:15:29 -0400

    fix: modal

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,26 +1,28 @@
-import { useState, useEffect, useRef } from 'react';
-import fs from 'fs';
-import matter from 'gray-matter';
-import Link from 'next/link';
-import SEO from '../components/SEO';
-import { useRouter } from 'next/router';
-import styles from '../styles/markdown.module.scss';
-import markdown from 'markdown-it';
-import Modal from 'react-modal';
-import Tags from '../components/Tags';
+import { useState, useEffect, useRef } from "react";
+import fs from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
+import SEO from "../components/SEO";
+import { useRouter } from "next/router";
+import styles from "../styles/markdown.module.scss";
+import markdown from "markdown-it";
+import Modal from "react-modal";
+import Tags from "../components/Tags";
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
+    maxWidth: "80vw",
+    maxHeight: "80vh",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
+  }
 };
 
-Modal.setAppElement('#modals');
+Modal.setAppElement("#modals");
 // Modal.setAppElement(document.getElementById("root"));
 
 export default function Home({ levers }: any) {
@@ -47,8 +49,8 @@ export default function Home({ levers }: any) {
   useEffect(() => {
     router.push(
       {
-        pathname: '/collection',
-        query: { lever: selectedLever?.title },
+        pathname: "/collection",
+        query: { lever: selectedLever?.title }
       },
       undefined,
       { shallow: true }
@@ -66,7 +68,7 @@ export default function Home({ levers }: any) {
   };
 
   const filterResults = (option) => {
-    if (option == 'Select') {
+    if (option == "Select") {
       setFilteredLevers(levers);
     } else if (!option) {
       setFilteredLevers(levers);
@@ -80,62 +82,62 @@ export default function Home({ levers }: any) {
   };
 
   return (
-    <div className='snap snap-mandatory snap-y'>
-      <SEO title='Home' />
-      <div className='flex flex-row border-b-[1px] border-black max-h-[96vh]'>
-        <div className='overflow-y-scroll border-r-[1px] border-black'>
-          <div className='w-full flex justify-between border-b-[1px] border-b-black'>
-            <Link href='/'>
+    <div className="snap snap-mandatory snap-y">
+      <SEO title="Home" />
+      <div className="flex flex-row border-b-[1px] border-black max-h-[96vh]">
+        <div className="overflow-y-scroll border-r-[1px] border-black">
+          <div className="w-full flex justify-between border-b-[1px] border-b-black">
+            <Link href="/">
               <img
-                alt=''
-                src='/icon-logo.png'
-                className='p-3 ml-4  h-10 w-auto'
+                alt=""
+                src="/icon-logo.png"
+                className="p-3 ml-4  h-10 w-auto"
               />
             </Link>
             {/* <div className="flex justify-end border border-black w-1/4 m-2 mr-8 p-1">
               <Image alt="" src="/icon-search.png" width={20} height={20} />
             </div> */}
           </div>
-          <div className='w-full h-full flex flex-row'>
-            <div className='w-[20vw] h-full overflow-hidden border-r-[1px] border-black p-8 flex flex-col gap-y-4 '>
+          <div className="w-full h-full flex flex-row">
+            <div className="w-[20vw] h-full overflow-hidden border-r-[1px] border-black p-8 flex flex-col gap-y-4 ">
               <div>
-                <p className='text-base'>Stage</p>
-                <label className='sr-only'>Underline select</label>
+                <p className="text-base">Stage</p>
+                <label className="sr-only">Underline select</label>
                 <select
                   value={selectedStage}
                   onChange={handleChange}
-                  id='underline_select'
-                  className='block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer'
+                  id="underline_select"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
                 >
                   <option selected>Select</option>
-                  <option value='Correction'>Correction</option>
-                  <option value='Funding'>Funding</option>
-                  <option value='Catalytic'>Catalytic</option>
-                  <option value='Procurement'>Procurement</option>
+                  <option value="Correction">Correction</option>
+                  <option value="Funding">Funding</option>
+                  <option value="Catalytic">Catalytic</option>
+                  <option value="Procurement">Procurement</option>
                 </select>
-                <p className='text-gray-500 my-10 text-sm'>
+                <p className="text-gray-500 my-10 text-sm">
                   Anyone can submit suggestions for new levers* or edits to
-                  existing ones through our public{' '}
+                  existing ones through our public{" "}
                   <a
-                    href='https://8fmdz3e9wad.typeform.com/to/Z1mOCaPD'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://8fmdz3e9wad.typeform.com/to/Z1mOCaPD"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     form
-                  </a>{' '}
-                  or{' '}
+                  </a>{" "}
+                  or{" "}
                   <a
-                    href='https://github.com/ansonyuu/levers'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://github.com/ansonyuu/levers"
+                    target="_blank"
+                    rel="noopener noreferrer"
                   >
                     Github
                   </a>
                   .
                 </p>
               </div>
-              <div className='mt-auto'>
-                <p className='text-gray-500 my-10 text-xs'>
+              <div className="mt-auto">
+                <p className="text-gray-500 my-10 text-xs">
                   *The inclusion of a lever does not necessarily constitute an
                   endorsement of it or reflect the work of our contributors.
                   None of the content on this site is meant to be an
@@ -144,27 +146,27 @@ export default function Home({ levers }: any) {
                 </p>
               </div>
             </div>
-            <div className='flex flex-col md:w-[50vw] overflow-y-scroll '>
+            <div className="flex flex-col md:w-[50vw] overflow-y-scroll ">
               {filteredLevers.map((lever, i) => {
                 return (
                   <div
                     key={i}
-                    className='grid items-start border-b-[0.5px]  border-b-black py-4 px-5 md:px-8 flex flex-col hover:bg-gray-100 cursor-pointer'
+                    className="grid items-start border-b-[0.5px]  border-b-black py-4 px-5 md:px-8 flex flex-col hover:bg-gray-100 cursor-pointer"
                   >
                     <div
                       onClick={() => handleClick(lever)}
-                      className='flex flex-row mb-2'
+                      className="flex flex-row mb-2"
                     >
-                      <h2 className='text-[30px] min-w-[50%]'>
+                      <h2 className="text-[30px] min-w-[50%]">
                         {lever?.title}
                       </h2>
 
-                      <p className=' text-[12px] min-w-[25%] max-w-[25%] '>
+                      <p className=" text-[12px] min-w-[25%] max-w-[25%] ">
                         {lever.oneliner}
                       </p>
 
-                      <div className='flex flex-row justify-end content-start items-start min-w-[25%] max-w-[25%] '>
-                        <div className='block justify-end'>
+                      <div className="flex flex-row justify-end content-start items-start min-w-[25%] max-w-[25%] ">
+                        <div className="block justify-end">
                           <Tags lever={lever} />
                         </div>
                       </div>
@@ -175,12 +177,12 @@ export default function Home({ levers }: any) {
             </div>
           </div>
         </div>
-        <div className='w-5/12 hidden md:inline-block overflow-y-scroll'>
-          <img alt='' src='/cover-market.png' className='' />
-          <div className='p-8'>
-            <h2 className='mt-3 my-3'>{selectedLever?.title} </h2>
+        <div className="w-5/12 hidden md:inline-block overflow-y-scroll">
+          <img alt="" src="/cover-market.png" className="" />
+          <div className="p-8">
+            <h2 className="mt-3 my-3">{selectedLever?.title} </h2>
 
-            <div className='flex flex-row mm-3 gap-2'>
+            <div className="flex flex-row mm-3 gap-2">
               <Tags lever={selectedLever} />
             </div>
 
@@ -188,28 +190,28 @@ export default function Home({ levers }: any) {
               className={styles.markdown}
               dangerouslySetInnerHTML={{
                 __html: md.render(
-                  selectedLever.content ? selectedLever.content : ''
-                ),
+                  selectedLever.content ? selectedLever.content : ""
+                )
               }}
             />
           </div>
         </div>
       </div>
-      <div id='modals' className='md:hidden'>
+      <div id="modals" className="md:hidden">
         <button onClick={openModal}>Open Modal</button>
         <Modal
           isOpen={modalIsOpen}
           onAfterOpen={afterOpenModal}
           onRequestClose={closeModal}
           style={customStyles}
-          contentLabel='Example Modal'
+          contentLabel="Lever Modal"
         >
-          <div className='w-5/12 hidden md:inline-block overflow-y-scroll'>
-            <img alt='' src='/cover-market.png' className='' />
-            <div className='p-8'>
-              <h2 className='mt-3 my-3'>{selectedLever?.title} </h2>
+          <div className="overflow-y-scroll">
+            <img alt="" src="/cover-market.png" className="" />
+            <div className="p-8">
+              <h2 className="mt-3 my-3">{selectedLever?.title} </h2>
 
-              <div className='flex flex-row mm-3 gap-2'>
+              <div className="flex flex-row mm-3 gap-2">
                 <Tags lever={selectedLever} />
               </div>
 
@@ -217,8 +219,8 @@ export default function Home({ levers }: any) {
                 className={styles.markdown}
                 dangerouslySetInnerHTML={{
                   __html: md.render(
-                    selectedLever.content ? selectedLever.content : ''
-                  ),
+                    selectedLever.content ? selectedLever.content : ""
+                  )
                 }}
               />
             </div>
@@ -230,11 +232,11 @@ export default function Home({ levers }: any) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync('levers');
+  const files = fs.readdirSync("levers");
   const levers: any = [];
 
   files.map((file) => {
-    const titlePost = fs.readFileSync(`levers/${file}`, 'utf-8');
+    const titlePost = fs.readFileSync(`levers/${file}`, "utf-8");
     const { data: postData, content } = matter(titlePost);
     console.log(levers);
     levers.push({
@@ -242,14 +244,14 @@ export async function getStaticProps() {
       oneliner: postData.oneliner,
       authors: postData.authors,
       stage: postData.stage,
-      link: postData.title ? `/?${postData.title.replace(/ /g, '%20')}` : '',
-      content: content,
+      link: postData.title ? `/?${postData.title.replace(/ /g, "%20")}` : "",
+      content: content
     });
   });
 
   return {
     props: {
-      levers,
-    },
+      levers
+    }
   };
 }
