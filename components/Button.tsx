a68cf61f5feab3618a9795468be518dbc71442bc
ansonyuu <ansonyu24@gmail.com>
2023-09-14 09:02:12 -0700

    fix: arrow alignment

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,16 @@ export default function Button({ className }: { className?: string }) {
         className={`relative rounded btn4 px-10 pt-3 pb-5 overflow-hidden bg-black ${className}`}
         type="button"
       >
-        <div className="flex flex-row gap-x-1 text-white">
+        <div className="flex flex-row gap-x-1 text-white align-baseline items-end">
           <span className="absolute inset-x-0 h-full bottom-0 bg-[#B8ADCB]"></span>
           <h3 className="z-10">Collection</h3>
-          <div className="bottom-0">
+          <div className=" ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="white"
               width="24"
               height="24"
-              className="z-10 bottom-0 right-0"
+              className=""
             >
               <path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z" />
             </svg>
