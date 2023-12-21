96e906d844bd1820e0c813efc80119ae3e3d53a2
Kelvin Yu <kelvin.yu.personal@gmail.com>
2023-12-22 00:58:00 +0100

    fix

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -102,7 +102,7 @@ export default function Letter() {
           trend. While the importance of scientists, entrepreneurs, and capital
           for innovation is obvious, we highlight the pivotal role that public
           policy and state capacity can have as well. LoP is an open collection
-          of <b>policies, tactics, and reforms</b>modern institutions have used
+          of <b>policies, tactics, and reforms</b> modern institutions have used
           to advance their scientific and technological output. Some have worked
           better than others, but we hope LoP will inspire more discussion on
           experimenting with similar interventions, faster and more often.
