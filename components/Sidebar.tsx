d473a4773cfae2ca4d897ed8622d8178da598bf1
Anson Yu <ansonyu24@gmail.com>
2024-01-05 13:08:45 -0800

    fix: sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Sidebar() {
     return (
         <div className="max-w-[12vw] fixed bottom-0 left-0 z-50 h-screen hidden md:inline-block gradient">
-            <div className="flex flex-col justify-end h-full ml-2 pb-4 mx-auto my-auto gap-y-1">
+            <div className="flex flex-col justify-end h-full ml-4 pb-6 mx-auto my-auto gap-y-1">
                 <div id="sidebarItem1" className="w-full">
                     <a href="/#section1">
                         <h5>Welcome</h5>
